30c9fe50e45fba6d9a926e812d579be9f0e92fa1
algass <algas.03@gmail.com>
2016-10-21 11:49:35 +0200

    Create formulaire.js

---
+++ tp3/formulaire.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+	  $("#formulaire.js").validate();
+});
+$("#nom").val()
+$("#prenom").val()
+$("#Date_de naissance").val()
+$("#address").val()
+$("#mail").val()
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+$('.modal')).modal('show');
+
+if{
+result<10 is value
+else{
+is false
+}
+}
