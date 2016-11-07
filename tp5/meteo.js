480420e1a67160d7913bb028130e840c00525e92
algass <algas.03@gmail.com>
2016-11-07 11:21:04 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -5,3 +5,42 @@ window.onload=function(){
     searchCity(city);
   });
 }
+function searchCity(_city){
+  console.log('searchCity',Hello from'+_city);
+    function searchCity(_city){
+    var request =new XMLHttpRequest();
+request.open('GET',
+             'http://api.openweathermap.org/data/2.5/weather?q=’+_city+’&appid=0ada432b59deb9716c357092c5f79be6',true);
+request.onload =function(){
+if(request.status >=200&& request.status <400){
+// Success!
+  var responseJSON = JSON.parse(request.responseText);
+  var temp = responseJSON.main.temp; // température en Kelvin!
+  var resp = request.responseText;
+  var icon =responseJSON weather.icon;
+  var temp =responseJSON weather.main;
+  var humidity = responseJSON.main.humidity;
+  var cloud = responseJSON. Cloudiness;
+  var wind = responseJSON.wind.speed;
+  
+//VOTRE CODE JS pour afficher les données météo sur votre page
+// en mettant à jour la DIV “result”
+}
+else{
+// We reached our target server, but it returned an error
+}
+};
+request.onerror =function(){
+// There was a connection error of some sort
+};
+    request.send();
+}         
+                      
+              
+              }
+function searchLatLng(_lat,_lng){
+    console.log(searchLatLng,'Hello from'+_lat+','+lng);
+    
+     
+    
+  }
