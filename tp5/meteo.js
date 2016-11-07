0ab0e278d1f1d5192f627997359f9864862093e1
algass <algas.03@gmail.com>
2016-11-07 10:12:32 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -1,1 +1,7 @@
 
+window.onload=function(){
+  document.getElementByld("searchCity").addEventListener("submit", function(){
+    var city=document.getElementByld("city").value;
+    searchCity(city);
+  });
+}
