73efe7a9e5120e035dc0b6962ae30fe96a676ff8
algass <algas.03@gmail.com>
2016-11-10 14:04:38 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,1 +1,14 @@
 
+function validation(){
+
+	var nom = document.getElementById("Nom").value.length;
+
+if (nom < 5){
+
+	document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+
+}
+
+	
+
+}
